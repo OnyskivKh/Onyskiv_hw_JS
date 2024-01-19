4b9debe1d39cef9908e76802aea3007a6403803c
“khrystynaonyskiv1209999@gmail.com” <khrystynaonyskiv1209999@gmail.com>
2024-01-19 18:02:02 +0100

    HW part 2 added:

--- Onyskiv_js_hw.js
+++ Onyskiv_js_hw.js
@@ -138,6 +138,102 @@ switch(mathOp) {
         break;
 }
 
+// 8. Використовуючи цикл for перемістити значення від 1 до 5 у масив myArray.
+
+let myArray = [];
+
+for(let i = 1; i <= 5; i++){
+    myArray.push(i);
+}
+console.log(myArray);
+
+// 9. Напишіть 2 цикли.
+//     За допомогою першого передайте парні числа від 1 до 9 у myArray за допомогою циклу for.
+//     За допомогою другого передайте непарні числа від 1 до 9 у myArray за допомогою циклу for.
+
+let myArray = []; // результат видасть непарні числа
+
+for (let i = 1; i <= 9; i++) {
+    if (i % 2) {
+        myArray.push(i);
+    }
+}
+console.log(myArray);
+
+///
+
+let myArray = []; // результат видасть парні числа
+
+for (let i = 1; i <= 9; i++) {
+    if (!(i % 2)) {
+        myArray.push(i);
+    }
+}
+console.log(myArray);
+
+// 10. Оголоcіть та ініціалізуйте змінну total = 0.
+// Використовуйте цикл for, щоб додати значення кожного елемента масиву myArr до підсумку.
+//     const myArr = [2, 3, 4, 5, 6];
+
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+
+for (let i = 0; i < myArr.length; i++) {
+    total += myArr[i];
+}
+
+console.log(total);
+
+
+// 11. Напишіть код, котрий виведе в консоль кожен піделемент в arr по одному.
+//
+//     const arr = [ [1, 2], [3, 4], [5, 6] ];
+//
+// Console output:
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+
+const arr = [ [1, 2], [3, 4], [5, 6] ];
+
+for(let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr[i].length; j++) {
+
+        console.log(arr[i][j]);
+    }
+
+}
+
+// 12. Доповніть код нижче, так щоб в консоль повертався результат множення усіх чисел у підмасивах arr.
+//     Використайте для цього вкладені цикли for.
+//
+// Наприклад:
+//     const arr = [[1], [2], [3]]  >  console.log(product) має повернути 6
+// const arr = [[1, 2], [3, 4], [5, 6, 7]]  >  console.log(product) має повернути 5040
+// const arr = [[5, 1], [0.2, 4, 0.5], [3, 9]]  >  console.log(product) має повернути 54
+//
+// const arr = [[1, 2], [3, 4], [5, 6, 7]];
+//
+// let product = 1;
+//
+// // тут має бути ваше рішення
+//
+// console.log(product);
+
+const arr = [[1, 2], [3, 4], [5, 6, 7]];
+
+let product = 1;
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        product *= arr[i][j];
+    }
+}
+console.log(product);
 
 // 13.*  Використовуючи властивості рядків (тип string), та регулярний вираз (regular expression) видалити голосні букви зі слова.
 
