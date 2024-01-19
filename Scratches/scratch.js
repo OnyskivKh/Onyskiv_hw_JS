b36166c861b9c2ea8e4e669d33adb8c162aa865b
“khrystynaonyskiv1209999@gmail.com” <khrystynaonyskiv1209999@gmail.com>
2024-01-19 15:13:22 +0100

    Notes added

--- Scratches/scratch.js
+++ Scratches/scratch.js
@@ -1,14 +1,1 @@
-let a = [1,2,3,4,5]
-let b = [];
 
-for(let i = 0; i < a.length; i++){
-
-    if(!(a[i]%2)){
-        b.push(a[i]);
-    }
-
-    for(let j = 0; j < b.length; j++) {
-        console.log(b[j]);
-    }
-
-}
